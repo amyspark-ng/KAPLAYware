76b8d8453f4bd6f613370bb31a5a27f56b523138
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-03-27 10:49:32 -0400

    add climb minigame

---
+++ games/dragoncoder047/climb.ts
@@ -0,0 +1,112 @@
+import { assets } from "@kaplayjs/crew";
+import { AreaComp, BodyComp, GameObj, PosComp } from "kaplay";
+import { Minigame } from "../../src/game/types";
+import mulfokColors from "../../src/plugins/colors";
+
+const smashGame: Minigame = {
+    prompt: "climb",
+    author: "dragoncoder047",
+    rgb: mulfokColors.BLACK,
+    input: { keys: { use: true } },
+    duration: 10,
+    urlPrefix: "games/dragoncoder047/assets/",
+    load(ctx) {
+        ctx.loadSprite("steel", assets.steel.sprite);
+        ctx.loadSprite("apple", assets.apple.sprite);
+        ctx.loadSound("jump", "/climb/jump.mp3");
+    },
+    start(ctx) {
+        const game2 = ctx.make([]);
+        game2.add([
+            ctx.pos(ctx.center().x, 20),
+            ctx.anchor("top"),
+            ctx.text("Get the apples!", { align: "center" }),
+        ]);
+        const level = ctx.addLevel([
+            "#############",
+            "#,         ,#",
+            "####     ####",
+            "#     @     #",
+            "#    ###    #",
+            "#,         ,#",
+            "#############",
+        ], {
+            tiles: {
+                "#": () => [
+                    ctx.sprite("steel"),
+                    ctx.body({ isStatic: true }),
+                    ctx.area(),
+                    ctx.anchor("center"),
+                    ctx.z(0),
+                    "grass",
+                ],
+                ",": () => [
+                    ctx.sprite("apple"),
+                    ctx.anchor("center"),
+                    ctx.body({ isStatic: true }),
+                    ctx.area(),
+                    ctx.opacity(1),
+                    ctx.rotate(0),
+                    ctx.z(1),
+                    "apple",
+                ],
+                "@": () => [
+                    ctx.sprite("@bean"),
+                    ctx.body(),
+                    ctx.area(),
+                    ctx.anchor("center"),
+                    ctx.z(2),
+                    "bean",
+                ],
+            },
+            tileWidth: 64,
+            tileHeight: 64,
+        });
+        const bean = level.get("bean")[0] as GameObj<PosComp | BodyComp | AreaComp>;
+        const endgame = () => {
+            z.cancel();
+            ctx.wait(2, () => {
+                ctx.finish();
+                level.children.forEach(x => x.paused = x.hidden = true);
+                level.destroy();
+            });
+        };
+        const z = ctx.onTimeout(() => {
+            ctx.lose();
+            endgame();
+        });
+        const SPEED = 300;
+        ctx.onButtonDown("left", () => {
+            bean.move(-SPEED, 0);
+        });
+        ctx.onButtonDown("right", () => {
+            bean.move(SPEED, 0);
+        });
+        ctx.onButtonPress("up", () => {
+            bean.jump();
+        });
+        bean.onCollide("apple", apple => {
+            ctx.burp();
+            apple.isStatic = false;
+            apple.collisionIgnore.push("*");
+            apple.jump(ctx.rand(200, 400));
+            apple.vel.x = ctx.rand(-300, 300);
+            const speed = ctx.rand(-100, 100);
+            apple.onUpdate(() => apple.angle += speed * ctx.dt());
+            apple.fadeOut(1).then(() => apple.destroy());
+            if (level.get("apple").filter(apple => apple.isStatic).length === 0) {
+                ctx.win();
+                endgame();
+            }
+        });
+        const apples = ctx.shuffle(level.get("apple"));
+        while (apples.length > ctx.difficulty) {
+            apples.pop()!.destroy();
+        }
+        level.pos = ctx.center().sub(level.tileWidth() * (level.numColumns() - 2) / 2, level.tileHeight() * level.numRows() / 2);
+        ctx.setGravity(2000);
+        return game2;
+    },
+};
+
+export default smashGame;
