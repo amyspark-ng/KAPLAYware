f3a895e2dd521ff054a16ffba969bee81ce7564c
lajbel <71136486+lajbel@users.noreply.github.com>
2025-07-22 00:33:06 -0300

    feat: desktop and load mods from desktop

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ import "./scenes/game/GameoverScene";
 
 import goGame from "../src/scenes/game/GameScene";
 
+import { filesystem, init } from "@neutralinojs/lib";
+
 k.setVolume(0.1);
 k.setCursor("none");
 k.loadRoot("./");
@@ -24,3 +26,29 @@ k.onLoad(() => {
 	if (k.isFocused()) INITIAL_SCENE();
 	else k.go("focus", INITIAL_SCENE);
 });
+
+// Init Neutralino API
+init();
+
+const loadMods = async () => {
+	try {
+		let entries = await filesystem.readDirectory(window.NL_PATH + "/mods/microgames/");
+
+		for (const entry of entries) {
+			if (entry.type != "DIRECTORY") return;
+
+			const customMicrogamesDirs = await filesystem.readDirectory(entry.path);
+
+			for (const microgameFolder of customMicrogamesDirs) {
+				const microgameMain = await filesystem.readFile(microgameFolder.path + "/main.js");
+				const microgameData = (await import(`data:text/javascript, ${microgameMain}`)).default;
+				window.microgames.push(microgameData);
+			}
+		}
+	}
+	catch (e) {
+		console.error("No mods folder found", e);
+	}
+};
+
+loadMods();
